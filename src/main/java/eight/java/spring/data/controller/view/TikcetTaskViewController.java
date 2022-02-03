fc83b170bef44befe90ff443bf05ca9a4cb64b53
reinnatan <reinnatan@gmail.com>
2022-02-03 23:24:10 +0800

    implement gmap and binding marker from database

--- src/main/java/eight/java/spring/data/controller/view/TikcetTaskViewController.java
+++ src/main/java/eight/java/spring/data/controller/view/TikcetTaskViewController.java
@@ -1,13 +1,31 @@
 package eight.java.spring.data.controller.view;
 
+import eight.java.spring.data.entity.TicketTask;
+import eight.java.spring.data.service.TicketTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class TikcetTaskViewController {
+
+    @Autowired
+    TicketTaskService ticketTaskService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        List<TicketTask> tickets = ticketTaskService.getAllTicketTasks();
+        model.addAttribute("ticketTasks",tickets);
         return "index";
     }
+
+    @GetMapping("/ticket-task-point")
+    public List<TicketTask> getTicketTask(){
+        return ticketTaskService.getAllTicketTasks();
+    }
+
 
 }
